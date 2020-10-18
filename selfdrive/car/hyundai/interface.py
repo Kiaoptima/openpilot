c4abaa4f76ad3cd768799fd3be5c979c2bdb87db
Kiaoptima <47659862+Kiaoptima@users.noreply.github.com>
2020-10-18 20:25:57 +0200

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -121,7 +121,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 15.25
       tire_stiffness_factor = 0.7
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.06]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
       ret.minSteerSpeed = 40 * CV.KPH_TO_MS
     elif candidate == CAR.KIA_STINGER:
       ret.mass = 1825. + STD_CARGO_KG
