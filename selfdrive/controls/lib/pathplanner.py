f42cb984da176e0295bec08e5ffbe6f22e0dab46
Kiaoptima <47659862+Kiaoptima@users.noreply.github.com>
2021-01-12 20:48:21 +0100

    Update pathplanner.py

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -113,7 +113,7 @@ class PathPlanner():
       torque_applied = sm['carState'].steeringPressed and \
                        ((sm['carState'].steeringTorque > 0 and self.lane_change_direction == LaneChangeDirection.left) or
                         (sm['carState'].steeringTorque < 0 and self.lane_change_direction == LaneChangeDirection.right)) or \
-                        CP.autoLcaEnabled and 2.25 > self.auto_lane_change_timer > 2. # 3.25 and 2. Default
+                        CP.autoLcaEnabled and 1.25 > self.auto_lane_change_timer > 1. # 3.25 and 2. Default
 
       blindspot_detected = ((sm['carState'].leftBlindspot and self.lane_change_direction == LaneChangeDirection.left) or
                             (sm['carState'].rightBlindspot and self.lane_change_direction == LaneChangeDirection.right))
