b2f2fcb92c08e3ffd37d08e1ed0f67dfccfd7a9c
Kiaoptima <47659862+Kiaoptima@users.noreply.github.com>
2021-01-13 18:53:46 +0100

    Update pathplanner.py

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -113,7 +113,7 @@ class PathPlanner():
       torque_applied = sm['carState'].steeringPressed and \
                        ((sm['carState'].steeringTorque > 0 and self.lane_change_direction == LaneChangeDirection.left) or
                         (sm['carState'].steeringTorque < 0 and self.lane_change_direction == LaneChangeDirection.right)) or \
-                        CP.autoLcaEnabled and 1.6 > self.auto_lane_change_timer > 1.1. # 3.25 and 2. Default
+                        CP.autoLcaEnabled and 3.25 > self.auto_lane_change_timer > 2. # 3.25 and 2. Default
 
       blindspot_detected = ((sm['carState'].leftBlindspot and self.lane_change_direction == LaneChangeDirection.left) or
                             (sm['carState'].rightBlindspot and self.lane_change_direction == LaneChangeDirection.right))
@@ -162,7 +162,7 @@ class PathPlanner():
     if self.lane_change_state == LaneChangeState.off:
       self.auto_lane_change_timer = 0.0
       self.prev_torque_applied = False
-    elif self.auto_lane_change_timer < 2.25: # stop afer 3 sec resume from 10 when torque applied. 3.25 Default
+    elif self.auto_lane_change_timer < 3.25: # stop afer 3 sec resume from 10 when torque applied. 3.25 Default
       self.auto_lane_change_timer += DT_MDL
 
     self.prev_one_blinker = one_blinker
