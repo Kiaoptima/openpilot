cd106aa9b32442806d04965aa1a0659e4e851398
Kiaoptima <47659862+Kiaoptima@users.noreply.github.com>
2021-01-21 16:56:33 +0100

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -124,7 +124,7 @@ class CarInterface(CarInterfaceBase):
     elif candidate in [CAR.KIA_OPTIMA, CAR.KIA_OPTIMA_HEV]:
       ret.mass = 3558. * CV.LB_TO_KG
       ret.wheelbase = 2.80
-      ret.steerRatio = 13.75 * 1.15
+      ret.steerRatio = 15.75
     elif candidate == CAR.KIA_STINGER:
       ret.mass = 1825. + STD_CARGO_KG
       ret.wheelbase = 2.78
