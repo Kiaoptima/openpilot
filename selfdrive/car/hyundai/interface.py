eecac085a63e2fcd4480381ca92a42ba8f3576b7
Kiaoptima <47659862+Kiaoptima@users.noreply.github.com>
2021-01-26 21:38:19 +0100

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -28,7 +28,7 @@ class CarInterface(CarInterfaceBase):
     # Most Hyundai car ports are community features for now
     ret.communityFeature = candidate not in [CAR.SONATA]
 
-    ret.steerActuatorDelay = 0.1  # 0.2 Default delay
+    ret.steerActuatorDelay = 0.2  # 0.2 Default delay
     ret.steerRateCost = 0.5 # 0.5 Default value
     ret.steerLimitTimer = 0.8 # 0.8 Default value
     tire_stiffness_factor = 1.
@@ -117,10 +117,10 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 1520. + STD_CARGO_KG
       ret.wheelbase = 2.80
-      ret.steerRatio = 13.75 # 13.75 default
-      tire_stiffness_factor = 0.7 # 0.7 default 
-      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
+      ret.steerRatio = 14.75 # 13.75 default
+      tire_stiffness_factor = 0.8 # 0.7 default 
+      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[9., 22.], [9., 22.]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2, 0.35], [0.05, 0.09]]
       ret.minSteerSpeed = 0 * CV.KPH_TO_MS
     elif candidate == CAR.KIA_STINGER:
       ret.mass = 1825. + STD_CARGO_KG
